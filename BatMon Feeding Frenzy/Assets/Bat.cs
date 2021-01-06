a3396a66f61ef153d10884ebf486e75bb7ca3f19
Joshua James <joshwa.james@gmail.com>
2021-01-06 14:39:42 -0700

    v1.0
    Initial version 1st level

---
+++ BatMon Feeding Frenzy/Assets/Bat.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bat : MonoBehaviour
+{
+    //Movement speed
+    public float speed = 2;
+
+    //Flap forece
+    public float force = 300;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Fly towardsd the right
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Flap
+        if (Input.GetKeyDown(KeyCode.Space))
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+    }
+}
---
+++ BatMon Feeding Frenzy/Assets/CameraFollow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //The Target
+    public Transform target;
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if(target == null) return;
+        transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+    }
+}
